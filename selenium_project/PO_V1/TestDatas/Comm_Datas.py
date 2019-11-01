df51e354f724fbc1f5ed4b27653254d2566d4292
Kk13579 <892336120@qq.com>
2019-11-01 16:21:58 +0800

    添加 Comm_Datas

---
+++ selenium_project/PO_V1/TestDatas/Comm_Datas.py
@@ -0,0 +1,41 @@
+# _*_ coding:utf-8 _*_
+# @Time    :2019/10/22 11:19
+# @Author  :xiuhong.guo
+# @Email   :892336120@qq.com
+# @File    :Comm_Datas.py
+
+# 前程贷的登录地址：http://120.78.128.25:8765/Index/login.html
+base_url='http://120.78.128.25:8765'
+
+# 登录地址
+web_login_url=base_url + "/Index/login.html"
+
+# 主页地址
+web_index_url=base_url + "/Index/index"
+
+# 通用普通账号-投资者
+user="18684720553"
+passwd="python"
+
+# 日志级别
+console_lev='DEBUG' # 设置输出到控制台的输出级别，相当于小开关
+log_lev='INFO' # 设置输出到文件的输出级别，相当于小开关
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
